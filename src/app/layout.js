f7d143369cffb0014af41e191db04604015f8d9b
Radek Włodarski <wlodarski.radek@gmail.com>
2023-11-28 22:28:51 +0100

    introduced TopBar to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import TopBar from "@/components/TopBar/page";
 
 const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <TopBar />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/TopBar/page.jsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import cameraImage from "../../resources/SVGs/video.svg";
+import Link from "next/link";
+
+export default function TopBar() {
+  return (
+    <div className="bg-black sticky top-0 left-0 right-0 p-6 z-10">
+      <div className="mx-auto flex flex-wrap items-center justify-evenly gap-x-20 gap-y-4 md:gap-y-6 max-w-7xl">
+        <div className="flex items-center gap-16 md:gap-24 text-white">
+          <Link href="/">
+            <div className="flex items-center gap-2.5 md:gap-4 hover:sepia">
+              <div className="flex items-center w-3.5 h-3 md:w-8 md:h-7">
+                <Image src={cameraImage} alt="logo" />
+              </div>
+              <h1 className="tracking-tighter font-medium text-sm md:text-2xl">
+                Movies Browser
+              </h1>
+            </div>
+          </Link>
+          <nav>
+            <ul className="flex gap-8 md:gap-11 text-xs md:text-sm font-semibold">
+              <li className="menuLink">MOVIES</li>
+              <li className="menuLink">PEOPLE</li>
+            </ul>
+          </nav>
+        </div>
+        <div className="flex justify-center w-96 bg-teal-600 text-white">
+          Search field
+        </div>
+      </div>
+    </div>
+  );
+}
