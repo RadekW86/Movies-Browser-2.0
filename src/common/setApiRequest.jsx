98158e967109793d63c3f9769aa3ce06d009ef43
Radek Włodarski <wlodarski.radek@gmail.com>
2023-11-30 22:23:52 +0100

    reorganized setApiRequest

--- src/common/setApiRequest.jsx
+++ src/common/setApiRequest.jsx
@@ -15,28 +15,16 @@ export default function setApiRequest() {
   switch (path[path.length - 1]) {
     case "Movies":
       if (search) {
-        url = `${baseURL}/search/movie?query=${search}&language=en-US&page=${
-          page ? page : "1"
-        }`;
         type = "MoviesSearch";
       } else {
-        url = `${baseURL}/movie/popular?language=en-US&page=${
-          page ? page : "1"
-        }`;
         type = "Movies";
       }
       break;
 
     case "People":
       if (search) {
-        url = `${baseURL}/search/person?query=${search}&language=en-US&page=${
-          page ? page : "1"
-        }`;
         type = "PeopleSearch";
       } else {
-        url = `${baseURL}/person/popular?language=en-US&page=${
-          page ? page : "1"
-        }`;
         type = "People";
       }
       break;
@@ -44,23 +32,40 @@ export default function setApiRequest() {
     default: {
       switch (path[path.length - 2]) {
         case "Movies":
-          url = `${baseURL}/movie/${path[path.length - 1]}?language=en-US`;
-          urlExtra = `${baseURL}/movie/${
-            path[path.length - 1]
-          }/credits?language=en-US`;
           type = "Movie";
           break;
         case "People":
-          url = `${baseURL}/person/${path[path.length - 1]}?language=en-US&`;
-          urlExtra = `${baseURL}/person/${
-            path[path.length - 1]
-          }/combined_credits?language=en-US&`;
           type = "Person";
           break;
         default:
           throw new Error();
       }
     }
+  }
+
+  switch (type) {
+    case "Movies":
+      url = `${baseURL}/movie/popular?language=en-US&page=${page ? page : "1"}`;
+      break;
+    case "MoviesSearch":
+      url = `${baseURL}/search/movie?query=${search}&language=en-US&page=${page ? page : "1"}`;
+      break;
+    case "People":
+      url = `${baseURL}/person/popular?language=en-US&page=${page ? page : "1"}`;
+      break;
+    case "PeopleSearch":
+      url = `${baseURL}/search/person?query=${search}&language=en-US&page=${page ? page : "1"}`;
+      break;
+    case "Movie":
+      url = `${baseURL}/movie/${path[path.length - 1]}?language=en-US`;
+      urlExtra = `${baseURL}/movie/${path[path.length - 1]}/credits?language=en-US`;
+      break;
+    case "Person":
+      url = `${baseURL}/person/${path[path.length - 1]}?language=en-US&`;
+      urlExtra = `${baseURL}/person/${path[path.length - 1]}/combined_credits?language=en-US&`;
+      break;
+    default:
+      throw new Error();
   }
 
   return { url, urlExtra, type };
