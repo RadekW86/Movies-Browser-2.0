94ca2a1432aea0a8e2416e48735fd9e0ee96c7fe
Radek Włodarski <wlodarski.radek@gmail.com>
2023-12-01 15:18:33 +0100

    styled Loading page

--- src/components/Loading/page.jsx
+++ src/components/Loading/page.jsx
@@ -3,8 +3,8 @@ import loadingCircle from "../../resources/SVGs/loadingCircle.svg";
 
 export default function Loading() {
   return (
-    <div>
-      <div>
+    <div className="flex items-center justify-center">
+      <div className="max-w-[35px] max-h-[35px] md:max-w-[91px] md:max-h-[91px] my-5 md:my-12 animate-spin">
         <Image src={loadingCircle} alt="Loading..." />
       </div>
     </div>
