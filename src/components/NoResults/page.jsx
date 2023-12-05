ef2f3e86d30f88ddae84e799901e6d224b0c9121
Radek Włodarski <wlodarski.radek@gmail.com>
2023-12-05 15:03:05 +0100

    created NoResults Page

---
+++ src/components/NoResults/page.jsx
@@ -0,0 +1,17 @@
+import getParams from "@/common/getParams";
+import Image from "next/image";
+import noResults from "../../resources/SVGs/noResults.svg";
+
+export default function NoResults() {
+  const { search } = getParams();
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h2 className="w-full my-3 font-semibold text-lg md:text-4xl">
+        Sorry, there are no results for “{search}”
+      </h2>
+      <div className="w-56 md:w-[668px]">
+        <Image priority src={noResults} alt="no results image" />
+      </div>
+    </div>
+  );
+}
