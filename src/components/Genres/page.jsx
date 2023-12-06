c4753abc34ace1467d6442956c89911b1258c750
Radek Włodarski <wlodarski.radek@gmail.com>
2023-12-06 22:21:51 +0100

    created Genres Component

---
+++ src/components/Genres/page.jsx
@@ -0,0 +1,23 @@
+import { useGenres } from "@/common/genresContext";
+
+export default function Genres({ genre_ids }) {
+  const { genresList } = useGenres();
+
+  const genresListFilteredByGenres =
+    genresList &&
+    genresList.genres.filter((genre) => genre_ids.includes(genre.id));
+
+  return (
+    <div className="flex flex-wrap gap-2 mb-3 md:mb-7 text-[10px] md:text-[13px]">
+      {genresList &&
+        genresListFilteredByGenres.map((item) => (
+          <div
+            className="bg-gray rounded-md py-1 px-1.5 md:py-1.5 md:px-2.5 text-center"
+            key={item.id}
+          >
+            {item.name}
+          </div>
+        ))}
+    </div>
+  );
+}
