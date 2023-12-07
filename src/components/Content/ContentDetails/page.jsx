59fc110774c9b698621c225371cc1877ced04992
Radek Włodarski <wlodarski.radek@gmail.com>
2023-12-07 23:15:38 +0100

    presented dataExtra on details Page

--- src/components/Content/ContentDetails/page.jsx
+++ src/components/Content/ContentDetails/page.jsx
@@ -1,24 +1,107 @@
+import { moviesPath, peoplePath } from "@/common/routes";
 import Loading from "@/components/Loading/page";
+import Section from "@/components/Section/page";
+import Tile from "@/components/Tile/page";
+import { nanoid } from "nanoid";
 
 export default function ContentDetails({
   status,
-  type,
   title,
   data,
   dataExtra,
+  atMovies,
+  atPeople,
 }) {
   console.log(data, dataExtra);
+  let category;
+  atMovies && (category = peoplePath);
+  atPeople && (category = moviesPath);
+
   return (
     <>
-      <h2>
-        {title[0]}, {title[1]}
-      </h2>
-      <div>{type}</div>
-      <div>{status}</div>
       {status === "success" ? (
-        <>
-          <div>OK</div>
-        </>
+        <main className="pt-6 md:py-11 flex flex-col justify-center items-center">
+          {dataExtra.cast.length > 0 ? (
+            <>
+              <h2 className="w-full p-1 md:p-3 font-semibold text-lg md:text-4xl">
+                {title[0]} ({dataExtra.cast.length})
+              </h2>
+              <Section category={category}>
+                {dataExtra.cast.map(
+                  ({
+                    id,
+                    poster_path,
+                    profile_path,
+                    title,
+                    name,
+                    release_date,
+                    genre_ids,
+                    vote_average,
+                    vote_count,
+                    character,
+                  }) => (
+                    <Tile
+                      key={id}
+                      category={category}
+                      id={id}
+                      poster_path={poster_path}
+                      profile_path={profile_path}
+                      title={title}
+                      name={name}
+                      release_date={release_date}
+                      genre_ids={genre_ids}
+                      vote_average={vote_average}
+                      vote_count={vote_count}
+                      role={character}
+                    />
+                  )
+                )}
+              </Section>
+            </>
+          ) : (
+            ""
+          )}
+          {dataExtra.crew.length > 0 ? (
+            <>
+              <h2 className="w-full p-1 md:p-3 font-semibold text-lg md:text-4xl">
+                {title[1]} ({dataExtra.crew.length})
+              </h2>
+              <Section category={category}>
+                {dataExtra.crew.map(
+                  ({
+                    id,
+                    poster_path,
+                    profile_path,
+                    title,
+                    name,
+                    release_date,
+                    genre_ids,
+                    vote_average,
+                    vote_count,
+                    job,
+                  }) => (
+                    <Tile
+                      key={nanoid()}
+                      category={category}
+                      id={id}
+                      poster_path={poster_path}
+                      profile_path={profile_path}
+                      title={title}
+                      name={name}
+                      release_date={release_date}
+                      genre_ids={genre_ids}
+                      vote_average={vote_average}
+                      vote_count={vote_count}
+                      role={job}
+                    />
+                  )
+                )}
+              </Section>
+            </>
+          ) : (
+            ""
+          )}
+        </main>
       ) : (
         <Loading />
       )}
